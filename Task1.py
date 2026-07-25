1b885fdebefb70c65e2696e3ca5acce07b8a0edf
Shiwendra <shiwendra@gmail.com>
2026-07-25 16:01:14 +0530

    Added new rout for greetings

--- Task1.py
+++ Task1.py
@@ -8,6 +8,9 @@ def home():
 @app.route('/health', methods=['GET'])
 def health_check():
     return jsonify({"status": "Application is running"})
+@app.route('/greet/<string:name>', methods=['GET', 'POST'])
+def greet(name):
+    return jsonify({"message": f"Hello, {name}!"})
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000, debug=True)
